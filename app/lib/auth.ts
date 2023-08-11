705177a8f8e229d6a3f88b4254f0797e99ad4da6
windrifter <jacobafarlow@gmail.com>
2023-08-11 09:15:15 -0400

    Wrote auth lib file
    So that env values can be used elsewhere as needed without needing to redeclare them in each location

---
+++ app/lib/auth.ts
@@ -0,0 +1,5 @@
+// using "as" here to get rid of default "string | undefined" type
+const CLOUD_NAME = process.env.CLOUD_NAME as string
+export const API_KEY = process.env.API_KEY as unknown as number
+export const API_SECRET = process.env.API_SECRET as string
+export const BASE_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}`
