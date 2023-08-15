0542ddb6a2d9fd68185508bf6e2641d220ce8d3a
windrifter <jacobafarlow@gmail.com>
2023-08-15 11:14:38 -0400

    Updates to the route for troubleshooting
    Using the route for testing out Cloudinary API hits

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,18 +1,25 @@
 import { NextResponse } from "next/server"
 import { BASE_URL, API_KEY, API_SECRET } from "../lib/auth"
 
-export async function GET(nextCursor?: string) {
+export async function GET() {
   const params = new URLSearchParams()
 
-  params.append('context', "true")
-  params.append('max_results', "2")
-  params.append('tags', "true")
-  if (nextCursor) {
-    params.append('next_cursor', nextCursor)
-  }
+  // params.append('context', "true")
+  // params.append('tags', "true")
+  params.append('max_results', "1")
+  params.append('expression', "birds")
+  
+  //* Need to include [] so that with_field is treated as an array, not a string
+  //* otherwise only the second with_field will be sent
+  params.append('with_field', "context")
+  params.append('with_field', "metadata")
+
+  // if (nextCursor) {
+  //   params.append('next_cursor', nextCursor)
+  // }
 
   //! In GET requests, there can be no body, so all params must be passed in as part of the URL
-  const res = await fetch(BASE_URL + '/resources/image' + `?${params}`, {
+  const res = await fetch(BASE_URL + '/resources/search' + `?${params}`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -20,7 +27,7 @@ export async function GET(nextCursor?: string) {
     },
   })
 
-  const images: Image[] = await res.json()
+  const responseJson = await res.json()
 
-  return NextResponse.json(images)
+  return NextResponse.json(responseJson)
 }
