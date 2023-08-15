099ab06f2bbda4488940923e4186af6a70924a71
windrifter <jacobafarlow@gmail.com>
2023-08-15 14:17:24 -0400

    Added explanatory comment for scientificRef

--- app/lib/scientificRef.ts
+++ app/lib/scientificRef.ts
@@ -2,6 +2,7 @@
 
 import parse from 'html-react-parser'
 
+// italicizes any text between parentheses in strings passed into this function
 export default function scientificRef(str:string) {
   return parse(str.replace(/\(/g, '<i>(').replace(/\)/g,")</i>"))
 }
