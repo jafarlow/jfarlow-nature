0ca52ba60ce40cb6714d9c4b12ba4d8425e26232
windrifter <jacobafarlow@gmail.com>
2023-08-17 09:50:26 -0400

    Outlining the About component
    Might need to make it a page? Still not really sure when to use components versus pages in Next

---
+++ app/components/About.tsx
@@ -0,0 +1,15 @@
+export default function About() {
+  return (
+    <>
+      <h2>Welcome!</h2>
+      <img src="" alt="" />
+      <p>My name is Jacob Farlow, and I am a nature photographer. Welcome to my photo gallery!</p>
+      <p>MOAR PLACEHOLDER</p>
+    </>
+  )
+}
+
+// layout:
+// Header
+// Photo -- perhaps randomized rotating image?
+// paragraphs
