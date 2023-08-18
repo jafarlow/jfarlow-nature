c1b0ebeae69413747df27daf323be96dab95cc9f
windrifter <jacobafarlow@gmail.com>
2023-08-18 09:42:04 -0400

    Tinkering with auth for dev work
    may need to revert changes once things get further along

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,5 +1,7 @@
 // using "as" here to get rid of default "string | undefined" type
-const CLOUD_NAME = process.env.CLOUD_NAME as string
+// const CLOUD_NAME = process.env.CLOUD_NAME as string
 export const API_KEY = process.env.API_KEY as unknown as number
 export const API_SECRET = process.env.API_SECRET as string
-export const BASE_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}`
+export const BASE_URL = `https://api.cloudinary.com/v1_1/drhoch1am`
+
+export const nextCursor = "c0748b7215f565694b552556d8fcbf2e9ccfda60b074d6c7082922556e9703416907e7e5f120723106ea6dd2be199197"
