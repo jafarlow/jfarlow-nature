866177b6623144d79a64eb56d81e6599495c0441
windrifter <jacobafarlow@gmail.com>
2023-08-18 09:44:46 -0400

    Added link to gallery route
    For testing the API call, which should run as part of loading the page
    Right now, the link works, but the calls are failing

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import styles from './page.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main>
+      <Link href="/gallery">View the gallery</Link>
     </main>
   )
 }
