0ff7b930acd5016a2d2fc6c84add861f525d1708
windrifter <jacobafarlow@gmail.com>
2023-08-24 14:20:23 -0400

    Update the route to handle optional param
    It took a bit of working to figure out how to access the input nextCursor from the req object, but I did it
     - use regex to snag the query param
     - drop the = from the return string
     - set the resulting value as the next_cursor for the url params

--- app/api/photos/route.ts
+++ app/api/photos/route.ts
@@ -1,11 +1,22 @@
 import { NextResponse } from "next/server"
 import { BASE_URL, API_KEY, API_SECRET } from "../../lib/auth"
+import { NextApiRequest } from "next"
 
-export async function GET() {
+export async function GET(req: NextApiRequest) {
   const params = new URLSearchParams()
 
   params.append('max_results', "1") //! UPDATE THIS # AFTER TESTING
   params.append('context', "true")
+  
+  //* to pull out the nextCursor value passed into the request
+  const regex = /=(.+)/; // match '=' and capture everything that follows
+  const matcher = req.url?.match(regex)
+  
+  if (matcher) {
+    params.append("next_cursor", matcher[0].slice(1)) // getting rid of the = from the string & assigning it to next_cursor
+  }
+
+  console.log("GET PARAMS: ", params)
 
   const res = await fetch(BASE_URL + '/resources/image' + `?${params}`, {
     method: "GET",
@@ -18,7 +29,7 @@ export async function GET() {
   const responseJson = await res.json()
 
   // see console for printout
-  console.log("CONTEXT: ", responseJson.resources[0].context)
-  console.log("METADATA: ", responseJson.resources[0].metadata)
+  // console.log("CONTEXT: ", responseJson.resources[0].context)
+  // console.log("METADATA: ", responseJson.resources[0].metadata)
   return NextResponse.json(responseJson)
 }
