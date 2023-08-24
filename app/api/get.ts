493a55a23dc834f891b01ffa8d914eb5ebe5cfee
windrifter <jacobafarlow@gmail.com>
2023-08-24 14:24:33 -0400

    Removed test code from /api
    The api directory had a few test files for troubleshooting calls. These were set up wrong, and in the wrong place to boot, so they get the axe

--- app/api/get.ts
+++
@@ -1,9 +0,0 @@
-import getImages from "../lib/getImages";
-import { NextApiResponse, NextApiRequest } from "next";
-
-export default async function handler(res: NextApiResponse, req: NextApiRequest) {
-  const params = JSON.parse(req.body)
-  const results = await getImages(params)
-
-  return res.status(200).json({ ...results })
-}
