0d0c55f07d81d23f95afcb03240353dd2a4e35d5
windrifter <jacobafarlow@gmail.com>
2023-08-24 14:18:43 -0400

    Updated getImages to pass through nextCursor
    fetch needs to be able to include nextCursor, which will either be "" or an alphanumeric string

--- app/lib/getImages.ts
+++ app/lib/getImages.ts
@@ -1,5 +1,12 @@
 export default async function getImages(nextCursor:string) {
-  const res = await fetch("../api/photos")
+  const params = new URLSearchParams()
+
+  if (nextCursor) {
+    params.append('next_cursor', nextCursor)
+  }
+
+  const res = await fetch("../api/photos" + `?${params}`)
   const resJson = await res.json()
+
   return resJson
 }
