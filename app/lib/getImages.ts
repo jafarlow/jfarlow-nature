b6b31af724deb86d7604c54ea9e577d0a624c063
windrifter <jacobafarlow@gmail.com>
2023-08-24 19:06:23 -0400

    Tinkering with error message
    Not anything really serious at the moment, but I do need to load up on the error handlers

--- app/lib/getImages.ts
+++ app/lib/getImages.ts
@@ -6,6 +6,10 @@ export default async function getImages(nextCursor:string) {
   }
 
   const res = await fetch("../api/photos" + `?${params}`)
+
+  // TODO: write a better error handler
+  if(!res.ok) throw new Error("PANIC AT THE GETIMAGES FUNCTION")
+
   const resJson = await res.json()
 
   return resJson
