4dc9e4991eca1c7325758e8c0087ec54fdde7f69
windrifter <jacobafarlow@gmail.com>
2023-08-24 19:07:51 -0400

    Swapped hardcoded test value for dynamic one
    Swapped out the hardcoded value from testing searchImages in order to test dynamic updating from state
     - this is working as expected now

--- app/lib/searchImages.ts
+++ app/lib/searchImages.ts
@@ -3,9 +3,10 @@ export default async function searchImages(searchValue:string, nextCursor?:strin
 
   params.append("expression", searchValue)
   if (nextCursor) {
-    params.append('next_cursor', "9a679844a7e3ccae541aa3ba8b7162aed1377829f985a07756fef96cf500480d")
+    params.append('next_cursor', nextCursor)
   }
 
+  //TODO: add error handling
   const res = await fetch("../api/search" + `?${params}`)
   const resJson = await res.json()
 
