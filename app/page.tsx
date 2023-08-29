bb9e2486bba9fc09902a8deb29f483d5a1324654
windrifter <jacobafarlow@gmail.com>
2023-08-29 16:25:02 -0400

    Imported new component to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Link from 'next/link'
+import HomeImage from './components/HomeImage'
 
 export default function Home() {
   return (
     <main className='home'>
       <p>Here's a bunch of placeholder text. I still need to work on a neato burrito intro to the site. Perhaps a nifty lil' profile image as well.</p>
+      <HomeImage />
       <Link href="/gallery" className="navlink">View the gallery</Link>
     </main>
   )
