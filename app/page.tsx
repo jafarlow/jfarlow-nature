151e7cdc5e3c88a133c388d92b0930eba62d2186
windrifter <jacobafarlow@gmail.com>
2023-08-30 16:45:03 -0400

    Fleshing out home page
    Intro text, which still needs refinement

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,20 @@ import HomeImage from './components/HomeImage'
 export default function Home() {
   return (
     <main className='home'>
-      <p>Here's a bunch of placeholder text. I still need to work on a neato burrito intro to the site. Perhaps a nifty lil' profile image as well.</p>
+      <h2>Welcome!</h2>
       <HomeImage />
-      <Link href="/gallery" className="navlink">View the gallery</Link>
+      <p>Few things have brought such joy to my life as being out in nature with a camera, and I'd like to share the highlights with you. This photo gallery represents the very best of my photography across seven categories:</p>
+      <ul className="home-tags">
+        <li>Aquatic creatures</li>
+        <li>Birds</li>
+        <li>Flora</li>
+        <li>Fungi</li>
+        <li>Invertebrates</li>
+        <li>Landscapes</li>
+        <li>Non-avian vertebrates</li>
+      </ul>
+      <p>I built this photo gallery as a way to blend Jacob as nature photographer with Jacob as software engineer.</p>
+      <Link href="/gallery" className="load-more home-button">View the gallery</Link>
     </main>
   )
 }
