88f9cca3c477168f1c98c8523acea76be5b651f8
windrifter <jacobafarlow@gmail.com>
2023-08-30 14:33:03 -0400

    Tinkering with Next/Image and typing
    Using new type to declare what to expect, as well as tinkering with next/image instead of just img

--- app/components/HomeImage.tsx
+++ app/components/HomeImage.tsx
@@ -1,23 +1,26 @@
 "use client"
+/* eslint-disable @next/next/no-img-element */
+
 import { useEffect, useState } from "react"
+import Image from "next/image"
 import profilePhotoData from "../../profilePhotoData.json"
 
 export default function HomeImage() {
-  const [profilePic, setProfilePic] = useState<{}>({})
-  const profile = profilePhotoData.photos 
+  // using ` | any` here to avoid error on `.path/alt/id` for the {} prior to loading anything into state
+  const [profilePic, setProfilePic] = useState<ProfilePic | any>({}) 
+  const photos: ProfilePic[] = profilePhotoData.photos 
 
   function getRandomInt(max: number) {
     return Math.floor(Math.random() * max);
   }
 
   useEffect(() => {
-    console.log(profile)
-    const num = getRandomInt(profile.length)
-    console.log("JUST ONE: ", profile[num])
-    setProfilePic(profile[num])
+    const num = getRandomInt(photos.length)
+    setProfilePic(photos[num])
   }, [])
 
   return (
-    <img src={profilePic.path} alt={profilePic.alt} key={profilePic.id}/>
+    // <img src={profilePic.path} alt={profilePic.alt} key={profilePic.id}/>
+    <Image src={profilePic.path || ""} alt={profilePic.alt || ""} key={profilePic.id} width={800} height={450} unoptimized={true} className="profile-pic"/>
   )
 }
