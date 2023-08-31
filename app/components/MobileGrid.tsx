57ad2e6400b4b83d555229c717c74d6da29ea216
windrifter <jacobafarlow@gmail.com>
2023-08-31 18:10:18 -0400

    Converted MobileGrid to use Next/Image
    Did this to enable using the blur property, as mobile is the scenario most likely to need it

--- app/components/MobileGrid.tsx
+++ app/components/MobileGrid.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 
+import Image from "next/image";
 import scientificRef from "../lib/scientificRef";
 
 type Props = {
@@ -11,7 +12,16 @@ export default function MobileGrid( {imageList}: Props ) {
     <div className="mobile-image-grid">
       {imageList.map((image) => (
         <figure className="mobile-figure" key={Math.random()}>
-          <img className="mobile-thumbnail" src={image.secure_url} alt={image.context?.custom?.alt || image.context?.alt || ""} />
+          <Image 
+            className="mobile-thumbnail" 
+            src={image.secure_url} 
+            alt={image.context?.custom?.alt || image.context?.alt || ""} 
+            width={400} 
+            height={266}       
+            unoptimized={true} 
+            placeholder="blur" 
+            blurDataURL={"data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFUlEQVR42mNkKHKsZyACMI4qpK9CAG4PC/9UjHYEAAAAAElFTkSuQmCC "} 
+          />
           <figcaption className="mobile-caption">{scientificRef(image.metadata?.caption) ?? ''}</figcaption>
         </figure>
       ))}
