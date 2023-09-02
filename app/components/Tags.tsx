fa7332cbe693abdf8da139c07b70d6b59e04693d
windrifter <jacobafarlow@gmail.com>
2023-09-01 21:09:19 -0400

    Switching from API tags to local tags
    Since I know there are only 7 tags for the whole project, it seems silly to force an API call to Cloudinary just to get them
    They're only used for pre-defined search options, so I can speed up browser paint by using a local tags.json file instead of waiting for a server response

--- app/components/Tags.tsx
+++ app/components/Tags.tsx
@@ -1,14 +1,18 @@
+import tags from "../../tags.json"
+
 type Props = {
-  tags: string[],
   checked: any[],
   updateTagSearch: (e: { target: { id: string; }; }) => void
 }
 
-export default function Tags({tags, checked, updateTagSearch}: Props) {
+export default function Tags({checked, updateTagSearch}: Props) {
+  // need to double call it for some odd reason...
+  const tagsList = tags.tags
+
   return (
     <div id="tags-wrapper">
-      {tags.map((tag:string) => (
-        <label key={Math.random()} htmlFor={tag} className={checked.includes(tag) ? "checked tag" : "tag"}>
+      {tagsList.map((tag:string, index) => (
+        <label key={index} htmlFor={tag} className={checked.includes(tag) ? "checked tag" : "tag"}>
           <input 
             type='checkbox'
             checked={checked.includes(tag)}
