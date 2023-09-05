32d540788b9d2568eb291d2ea5f50cdd9c0a852c
windrifter <jacobafarlow@gmail.com>
2023-09-05 09:50:26 -0400

    Filling out more content in About

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -8,21 +8,32 @@ export default function About() {
       <h2>About the Project</h2>
 
       {/* TODO: link these to their subsections */}
-      <p>This project represents two parts of myself: Jacob as Nature Photographer, and Jacob as Software Engineer.</p>
+      <p>This project represents two parts of myself: Jacob as Nature Photographer, and Jacob as Software Engineer. What follows is a bit of autobiographical waxing poetic combined with being overly verbose.</p>
 
-      <h3>Jacob as Nature Photographer</h3>
+      <h3>Jacob as Nature Photographer--from the early days to now</h3>
       <h4>HEADING: the beginnings</h4>
       <p>My photography journey began when I was about 8 years old, though admittedly, it was probably more of a way for my parents to help me expend energy on family vacations. Prior to our departure, my folks would pack a treat bag full of snacks and puzzle games that were to last us the duration of whatever vaction we were embarking upon, and this included a disposable Kodak camera.</p> 
-      <p>Over many trips and the efforts of my mother to turn these rolls into scrapbooks, my folks must have seen something among the mess that resulted from a small rambunctious child given free reign to photograph whatever caught his eye. It was round about when I was 14 that my parents upgraded me from disposable Kodak to digital point-and-shoot.</p>
+      <p>Over many trips and the efforts of my mother to turn these rolls into scrapbooks, my folks must have seen something among the mess that resulted from a small rambunctious child given free reign to photograph whatever caught his eye. It was 'round about when I was 14 that my parents upgraded me from disposable Kodak to an Olympus digital point-and-shoot.</p>
 
       <h4>HEADING: There might just be something in this here hobby</h4>
       <p>TOPIC: photo contest ski slope & Cedarock Park waterfall + painting</p>
+      <p>Not long after this, I had the opportunity to bring this camera with me on the ski slopes of West Virginia's Winterplace ski resort. After tackling one of the runs, I turned around to look back up and thought to myself that it would make a pretty picture. I fell on my stomach, pulled out the camera, and snapped this photo:</p> {/* INSERT PHOTO "/My Pictures/ski trip/feb 7 05 (2)" */}
+      <p>I entered this photo into a local arts competition and took first prize, to my utter astonishment and delight. This moment, more than anything, triggered my desire to incporporate photography into my definition of self.</p>
+      <p>A few months after, I was taking a hike in one of my favorite places on the planet--Cedarock Park in Burlington, North Carolina. It holds a special place in my heart for many reasons, and one relevant to this project is it's where I learned that my "photographic eye" wasn't a fluke on the ski slopes. I took a photo of the park's iconic dam-<i>cum</i>-waterfall that cemented in my mind that landscapes and features within nature would be my forte.</p> {/* INSERT PHOTO "/My Pictures/fire/stuff 010" */}
+      <p>A few short years later, as a part of an art class we were directed to reproduce an image as a painting in the pointilist style. Without hesitation, I chose that photo from Cedarock Park as my subject.</p>
       <p>TOPIC: carrying it with me everywhere</p>
+      <p>The beautiful simplicity of a digital point-and-shoot camera is how easy it is to carry everywhere. I developed a habit of sacrificing one of my pants pockets to hold this camera every single day. One might think this would lead to an abundance of photos from daily life, however that isn't how I viewed photography--I saw it as an opportunity to record the unusual, the special, the unexpected. While this means I carried it always, and used it rarely, it did mean I was prepared when in chemisty class we learned about surface area, licopodium powder, and explosions.</p>{/* INSERT PHOTO "/My Pictures/fire/P1010170" */}
+      <p>In this moment I unlocked a new aspect of my photography: capturing moments of motion. It would take some dabbling in sports photography and plenty of "almost got it" experiences over the subsequent years before I would combine "movement" with "nature". But let's don't get ahead of the story.</p>
 
       <h4>HEADING: taking the (financial) plunge</h4>
-      <p>TOPIC: borrowing Barbar's camera & upgrading to DSLR</p>
+      <p>TOPIC: borrowing Barbara's camera & upgrading to DSLR</p>
+      <p>The seed was planted about upgrading my camera once again when a family friend learned of my interest in photography. She had a DSLR (digital single-lens reflex) camera, and when my family went for a visit she suggested I give her camera a try. Aside from the joy in trying out a new camera, I was honored that someone would trust 16 year old me with such an expensive piece of equipment. I spent an entire afternoon traipsing about their garden and forested property delighting in the new views I could explore. It would take another 3 years, but I took that financial plunge and purchased a Canon 40D, with a 35-128mm telephoto lens. This was one model removed from state of the art in Canon's lineup (at the time), with the basic all-around lens for someone on a budget who wants to diversify their subject matter. (A quick aside on telephoto lenses -- these are lenses which allow for manual zooming, which is to say the shape of the lens changes & elongates by hand, so the numbers 35-128 means non-zoomed the lens focal length is 35mm and at full zoom it'll be 128mm). An SLR or DSLR camera is special in that it allows for interchangeable lenses, enabling people to adjust their focus (pun absolutely intended!) and reach. Without needing to buy a whole new camera, a photographer can swap out compatible lenses.</p>
       <p>TOPIC: Italy/Switzerland</p>
+      <p>Just like breaking in running shoes before a race, I learned the hard way that I should have had taken the same approach with this camera. A week after it arrived, we hopped on an airplane to visit Japan. Aside from having a wonderful time as a tourist, my photographic foray there was, predicatbly, about on par with my old point-and-shoot. Thankfully, I had a blast with it the whole time, and so rather than be discouraged I was motivated to improve.</p>
+      <p>I had the opportunity of a lifetime to study for a semester in Italy, with an embarrassment of riches of subject matter: architecture, art, gardens, ruins, vistas. It was during this time that my landscape photography really began to establish itself as a cornerstone of my skillset. The majority of my travels during this time kept me in Italy, though I did take a jaunt up to Switzerland to knock off a bucket list item of hitting the slopes of the Swiss Alps. I took a big risk by bringing my camera with me; afterall, it's a large piece of precision equipment, and knocking it around is not generally advisable. The risk paid off in a big way, giving me both my greatest moment in action photography (see below), as well as in landscape photography (see landscapes in the gallery).</p>
+      {/* INSERT PHOTO "/My Pictures/Portfolio Updated 8-5-2015/Sports and Action/Interlaken/IMG_3484" */}
       <p>TOPIC: travels, photo subjects/targets, successes & frustrations</p>
+      <p>Hitting it big right off the bat like this (only 6 months of owning the camera) set myself up to be humbled, experiencing my own version of the Dunning-Kruger Effect. While I saved my ego by not broadcasting to others how great I thought myself, I fell victim to overconfidence internally, and so when subsequent forays into beautiful places didn't render the same astounding results I had some emotional damage control to do. Intense self reflection combined with treating each excursion as a learning opportunity righted the ship, so to speak, and quickly converted frustrations into improvements.</p>
       <p>TOPIC: BBC docs</p>
       <p>TOPIC: borrowing Henry's lens</p>
 
@@ -30,7 +41,7 @@ export default function About() {
       <p>TOPIC: new lens, new me</p>
       <p>TOPIC: the journey since</p>
 
-      <h3>Jacob as Software Engineer</h3>
+      <h3>Jacob as Software Engineer--no early days, just now</h3>
       <h4>HEADING: engineering overview</h4>
       <p>TOPIC: type of coding I do</p>
       <p>TOPIC: searching for a project to build as practice</p>
@@ -41,6 +52,7 @@ export default function About() {
       <p>TOPIC: Cloudinary API & host</p>
       <p>TOPIC: Nextjs framework + TS</p>
       <p>TOPIC: serverless functions</p>
+      <p>TOPIC: Overwriting ImageViewer to be accessible</p>
 
 
       <Link href="/gallery" className="load-more home-button">View the gallery</Link>
