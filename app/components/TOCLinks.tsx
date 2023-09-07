46e7154f7dfe99365f64e7f11af30a68db585969
windrifter <jacobafarlow@gmail.com>
2023-09-07 16:23:56 -0400

    Created TableOfContents for the About page
    To improve page nav

---
+++ app/components/TOCLinks.tsx
@@ -0,0 +1,84 @@
+import Link from "next/link";
+
+//* These are in alphabetical order, not in document order
+
+export const beginnings = (
+  <Link 
+    href="/about#nature-photographer" 
+    className="about-link" rel="internal"
+  >
+    The Beginnings
+  </Link>
+)
+
+export const connection = (
+  <Link 
+    href="/about#connection" 
+    className="about-link" rel="internal"
+  >
+    Connecting with nature
+  </Link>
+)
+
+export const engineering = (
+  <Link 
+    href="/about#software-engineer" 
+    className="about-link" rel="internal"
+  >
+    Engineering overview
+  </Link>
+)
+
+export const hobby = (
+  <Link 
+    href="/about#hobby" 
+    className="about-link" rel="internal"
+  >
+    There just might be something in this here hobby
+  </Link>
+)
+
+export const nextjs = (
+  <Link 
+    href="/about#nextjs" 
+    className="about-link" rel="internal"
+  >
+    Nextjs framework
+  </Link>
+)
+
+export const packages = (
+  <Link 
+    href="/about#packages" 
+    className="about-link" rel="internal"
+  >
+    There's a package for that
+  </Link>
+)
+
+export const plunge = (
+  <Link 
+    href="/about#plunge" 
+    className="about-link" rel="internal"
+  >
+    Taking the (financial) plunge
+  </Link>
+)
+
+export const plungeRedux = (
+  <Link 
+    href="/about#plunge-redux" 
+    className="about-link" rel="internal"
+  >
+    Taking the (financial) plunge redux
+  </Link>
+)
+
+export const portfolio = (
+  <Link 
+    href="/about#portfolio" 
+    className="about-link" rel="internal"
+  >
+    Beginning to build this portfolio
+  </Link>
+)
