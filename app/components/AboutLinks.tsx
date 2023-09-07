8a1c1101b187514aa4c3fb0e1b987ed1510b7189
windrifter <jacobafarlow@gmail.com>
2023-09-07 08:36:11 -0400

    Reorganized AboutLinks
    Added a few mew ones, and alphabetized them within subsections

--- app/components/AboutLinks.tsx
+++ app/components/AboutLinks.tsx
@@ -1,13 +1,6 @@
 import Link from "next/link";
 
-export const dkEffect = (
-  <Link 
-    href="https://en.wikipedia.org/wiki/Dunning%E2%80%93Kruger_effect" 
-    className="about-link" target="_blank" rel="external"
-  >
-    Dunning-Kruger Effect.
-  </Link>
-)
+//* INTERNAL:
 
 export const jasNP = (
   <Link 
@@ -24,6 +17,58 @@ export const jasSE = (
     className="about-link" rel="internal"
   >
     Jacob as Software Engineer
+  </Link>
+)
+
+//* EXTERNAL:
+
+export const dkEffect = (
+  <Link 
+    href="https://en.wikipedia.org/wiki/Dunning%E2%80%93Kruger_effect" 
+    className="about-link" target="_blank" rel="external"
+  >
+    Dunning-Kruger Effect.
+  </Link>
+)
+
+//* Software related:
+
+export const chrisBlakely = (
+  <Link 
+    href="https://www.youtube.com/watch?v=KsjixiVhVyY" 
+    className="about-link" target="_blank" rel="external"
+  >
+    video from Chris Blakely
+  </Link>
+)
+
+export const serverlessFunction = (
+  <Link 
+    href="https://vercel.com/docs/functions/serverless-functions" 
+    className="about-link" target="_blank" rel="external"
+  >
+    Vercel puts it:
+  </Link>
+)
+
+
+//* Natural history related:
+
+export const autumnwatch = (
+  <Link 
+    href="https://www.bbc.co.uk/programmes/b0079t1p" 
+    className="about-link" target="_blank" rel="external"
+  >
+    <i>Autumnwatch</i>
+  </Link>
+)
+
+export const bbcNHU = (
+  <Link 
+    href="https://en.wikipedia.org/wiki/BBC_Studios_Natural_History_Unit_filmography" 
+    className="about-link" target="_blank" rel="external"
+  >
+    <i>hundreds</i> of these!
   </Link>
 )
 
@@ -45,29 +90,11 @@ export const springwatch = (
   </Link>
 )
 
-export const autumnwatch = (
-  <Link 
-    href="https://www.bbc.co.uk/programmes/b0079t1p" 
-    className="about-link" target="_blank" rel="external"
-  >
-    <i>Autumnwatch</i>
-  </Link>
-)
-
 export const winterwatch = (
   <Link 
     href="https://www.bbc.co.uk/programmes/p012msk2" 
     className="about-link" target="_blank" rel="external"
   >
     <i>Winterwatch</i>
-  </Link>
-)
-
-export const serverlessFunction = (
-  <Link 
-    href="https://vercel.com/docs/functions/serverless-functions" 
-    className="about-link" target="_blank" rel="external"
-  >
-    Vercel puts it:
   </Link>
 )
