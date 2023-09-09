802d784e18407dad26d29c8766dafac20ee97dbf
windrifter <jacobafarlow@gmail.com>
2023-09-08 20:14:22 -0400

    Applying about style to home page
    For improved readability I've made the home page resemble the styling I have for content blocks
    Added more explanatory text to the home page
     - will probs need some adjustments here
    
    Updated CSS to account for name changes in classes
     - will need further cleanup to eliminate duplication and deprecated overrides
    
    Minor grammatical update for profilePhotoData

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ import HomeImage from './components/HomeImage'
 export default function Home() {
   return (
     <main className='home'>
+      <section className="content-block">
       <h2>Welcome!</h2>
+
       <HomeImage />
       <p>Few things have brought such joy to my life as being out in nature with a camera, and I'd like to share the highlights with you. With birds as my favored subject I have elected to separate them into their own category, and so this photo gallery, which represents the very best of my photography, is split into seven categories:</p>
         <ul className="home-tags">
@@ -16,7 +18,12 @@ export default function Home() {
           <li>Landscapes</li>
           <li>Non-avian vertebrates</li>
         </ul>
+
+      <p>A few quick things to note: This portfolio was designed to be viewed on desktop, tablet, and mobile. Desktop and tablet can view images in full screen, and the maximum resolution I have set for these images is 1620 x 1080. Sorry folks with 4k monitors, I don't have the capacity for the kind of cloud storage at the moment which would leverage all your screen real estate. Perhaps when I become a rich and famous photographer (I can dream, right?) I'll be able to serve up the photos in their original resolution.</p>
+
+      <p>Mobile device screen sizes are too small for a full screen experience to make much sense, so y'all get</p>
       <p>Use the link below to take a look!</p>
+      </section>
       <Link href="/gallery" className="load-more home-button">View the gallery</Link>
     </main>
   )
