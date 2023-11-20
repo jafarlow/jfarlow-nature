ebe113035908c3abf9ba93a5308adeb116383021
windrifter <jacobafarlow@gmail.com>
2023-11-20 14:52:03 -0500

    Incorporating image title to desktop view
    To make the gallery seem less bland, and to provide a bit more info on each image prior to any user interaction I've decided to include a title for each image
     - title value is set in Cloudinary console and served as "caption" within the metadata (even though their UI calls it "title")
    
    Opted for div -> img -> footer pattern as a title didn't feel proper setting it as a figcaption (figure -> img -> figcaption), and using an h* would be wrong

--- app/components/ImageGrid.tsx
+++ app/components/ImageGrid.tsx
@@ -12,17 +12,21 @@ export default function ImageGrid( {imageList, openImageViewer, keyboardNav}: Pr
       <p className="instruction">Select an image to view it in full screen</p>
       <div className="image-grid">
         {imageList.map((image, index) => (
-          <img 
-            className='thumbnail'
-            src={image.secure_url} 
-            alt={image.context?.custom?.alt || image.context?.alt || ""} 
-            onClick={ () => openImageViewer(index) }
-            onKeyDown={ (event) => keyboardNav(event,index)}
-            key={image.asset_id}
-            tabIndex={0}
-            width={356}
-            height={238}
-          />
+          <div className="img-card" key={image.asset_id}>
+            <img 
+              className='thumbnail'
+              src={image.secure_url} 
+              alt={image.context?.custom?.alt || image.context?.alt || ""} 
+              onClick={ () => openImageViewer(index) }
+              onKeyDown={ (event) => keyboardNav(event,index)}
+              // key={image.asset_id}
+              tabIndex={0}
+              width={356}
+              height={238}
+            />
+            {/* Cloudinary calls shows "title" in their UI, but serves it as "caption" in the custom metadata */}
+            <footer className="img-footer">{image.context?.custom?.caption || image.context?.caption}</footer>
+          </div>
         ))}
       </div>
     </div>
