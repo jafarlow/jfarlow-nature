d5730883f5b92f74daaf263256240e1b36bb4405
windrifter <jacobafarlow@gmail.com>
2023-12-11 16:35:00 -0500

    Partway to setting up accordion

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -10,14 +10,17 @@ type Props = {
 
 export default function Form( { checked, handleFormSubmit, updateTagSearch, clearSelection, resetSearch}: Props ) {
   return (
-    <form onSubmit={handleFormSubmit} id="tag-search" className='tags-form'>
-      <label htmlFor="tagsearch" id="form-label">Filter by category:</label>
-      <Tags checked={checked} updateTagSearch={updateTagSearch}/>
-      <div className="buttons-wrapper">
-        <button type="submit" id="submit">Confirm selection</button>
-        <button type="button" id="clear" onClick={clearSelection}>Clear selection</button>
-        <button type="button" id="reset" onClick={resetSearch}>Reset page</button>
-      </div>
-    </form>
+    <>
+      <button id="accordion-trigger" aria-expanded="false" aria-controls="tag-search" type="button">View categories:</button>
+      <form onSubmit={handleFormSubmit} id="tag-search" className='tags-form' role="region" aria-labelledby="accordion-trigger">
+        <label htmlFor="tagsearch" id="form-label">Filter by category:</label>
+        <Tags checked={checked} updateTagSearch={updateTagSearch}/>
+        <div className="buttons-wrapper">
+          <button type="submit" id="submit">Confirm selection</button>
+          <button type="button" id="clear" onClick={clearSelection}>Clear selection</button>
+          <button type="button" id="reset" onClick={resetSearch}>Reset page</button>
+        </div>
+      </form>
+    </>
   )
 }
