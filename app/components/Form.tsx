5f0739369357e6e7f11f0ce07c1c41a83b2a1bff
windrifter <jacobafarlow@gmail.com>
2023-12-19 16:29:00 -0500

    Added state elements for accordion button
    Toggle "view" & "hide" in button text
    Toggle right arrow & down arrow in button text
    
    Updated button text to reference state values instead of explicit string text

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -13,9 +13,19 @@ type Props = {
 
 export default function Form( { checked, handleFormSubmit, updateTagSearch, clearSelection, resetSearch}: Props ) {
   const [isExpanded, setIsExpanded] = useState(false);
+  const [buttonText, setButtonText] = useState("View")
+  const [buttonTextCode, setButtonTextCode] = useState(11208)
+  // UTF-8 decimal values for stylized arrows: 11208 for right arrow, 11206 for down arrow
+  // https://www.w3schools.com/charsets/ref_utf_arrows.asp
 
   const handleHiddenChange = () => {
     setIsExpanded(!isExpanded)
+    
+    if (buttonText === "View") setButtonText("Hide")
+    else setButtonText("View")
+
+    if (buttonTextCode === 11208) setButtonTextCode(11206)
+    else setButtonTextCode(11208)
   }
 
   return (
@@ -27,7 +37,7 @@ export default function Form( { checked, handleFormSubmit, updateTagSearch, clea
         type="button" 
         onClick={handleHiddenChange}
       >
-        View categories:
+        {buttonText} categories {String.fromCharCode(buttonTextCode)}
       </button>
       <form onSubmit={handleFormSubmit} id="tag-search" className='tags-form' role="region" aria-labelledby="accordion-trigger"
         aria-hidden={!isExpanded}>
