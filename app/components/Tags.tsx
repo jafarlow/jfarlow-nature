f21c422b0628c59a0539a8d32c51f08a0e3e0ca1
windrifter <jacobafarlow@gmail.com>
2023-12-22 17:13:12 -0500

    General cleanup
    killed an unused component that was a precursor to the About page
    
    Removed comments/dead code from css
    
    Replaced a JS expression with a static value, as the JS isn't needed in that situation

--- app/components/Tags.tsx
+++ app/components/Tags.tsx
@@ -9,6 +9,7 @@ export default function Tags({checked, updateTagSearch}: Props) {
   // need to double call it for some odd reason...
   const tagsList = tags.tags
 
+  // using index as key here because the list is static, and its order will not change. Otherwise an alternative option would be required
   return (
     <div id="tags-wrapper">
       {tagsList.map((tag:string, index) => (
@@ -16,7 +17,7 @@ export default function Tags({checked, updateTagSearch}: Props) {
           <input 
             type='checkbox'
             checked={checked.includes(tag)}
-            key={Math.random()}
+            key={index}
             name={tag}
             id={tag}
             onChange={updateTagSearch}
