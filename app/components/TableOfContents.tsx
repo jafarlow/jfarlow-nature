09d6d2e406437a2dc3665e3c767c865dbb0e9716
windrifter <jacobafarlow@gmail.com>
2024-02-16 11:52:48 -0500

    Fixed improper list nesting
    I had a ul with another ul as a direct child. Proper formatting would have that nested within an li
    
    wrong:
    ul
     li
     ul
      li
      li
    
    correct:
    ul
     li
      ul
       li
       li

--- app/components/TableOfContents.tsx
+++ app/components/TableOfContents.tsx
@@ -7,24 +7,26 @@ export default function TableOfContents() {
       <p>I'm not kidding when I mention this being overly verbose. The tl;dr (too long; didn't read) is that photography has been a part of my life for 25 years, and I'm a frontend software engineer who built this project from scratch. This portfolio is built with Next.js and Typescript using Cloudinary as data host with Vercel as site host.</p>
       <p>The tl;sr (too long; still read) can be navigated using the outline below:</p>
       <ul className="about-list">
-        <li>{jasNP}</li>
-        <ul>
-          <li>{beginnings}</li>
-          <li>{hobby}</li>
-          <li>{plunge}</li>
-          <li>{connection}</li>
-          <li>{plungeRedux}</li>
-          <li>{ids}</li>
-          <li>{theEnd}</li>
-        </ul>
-        <li>{jasSE}</li>
-        <ul>
-          <li>{engineering}</li>
-          <li>{portfolio}</li>
-          <li>{packages}</li>
-          <li>{nextjsSection}</li>
-          <li>{wrapup}</li>
-        </ul>
+        <li>{jasNP}
+          <ul>
+            <li>{beginnings}</li>
+            <li>{hobby}</li>
+            <li>{plunge}</li>
+            <li>{connection}</li>
+            <li>{plungeRedux}</li>
+            <li>{ids}</li>
+            <li>{theEnd}</li>
+          </ul>
+        </li>
+        <li>{jasSE}
+          <ul>
+            <li>{engineering}</li>
+            <li>{portfolio}</li>
+            <li>{packages}</li>
+            <li>{nextjsSection}</li>
+            <li>{wrapup}</li>
+          </ul>
+        </li>
       </ul>
       <p>Happy reading!</p>
     </>
