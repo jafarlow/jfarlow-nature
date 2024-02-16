afd8b99461a20235b26348935a7cec1c04dc0b70
windrifter <jacobafarlow@gmail.com>
2024-02-16 11:48:09 -0500

    Removed some unhelpful commentary on home page

--- app/page.tsx
+++ app/page.tsx
@@ -19,10 +19,8 @@ export default function Home() {
           <li>Non-avian vertebrates</li>
         </ul>
 
-      <p>A few quick things to note: This portfolio was designed to be viewed on desktop, tablet, and mobile. Desktop and tablet can view images in full screen, with a photo caption overlaid atop the image. This overlay can be removed by shifting the focus to one of the navigational arrows, and can be reapplied by hovering over the image, or shifting the focus back to the image using the tab key, or by clicking on the image itself.</p> 
+      <p>This portfolio was designed to be viewed on desktop, tablet, and mobile. Desktop and tablet have the option to view images in full screen, with a photo caption overlaid atop the image. This overlay can be removed by shifting the focus to one of the navigational arrows, and can be reapplied by hovering over the image, or shifting the focus back to the image using the tab key, or by clicking on the image itself.</p> 
       
-      <p>The maximum resolution I have set for these images is 1620 x 1080. Sorry folks with 4k monitors, I don't have the capacity for the kind of cloud storage at the moment which would leverage all your screen real estate. Perhaps when I become a rich and famous photographer I'll be able to serve up the photos in their original resolution. I can dream, right?</p>
-
       <p>As mobile screen sizes are restrictive, that full screen view described above would be detrimental to the overall user experience, so the captions are listed below their respective images. Desktop & tablet users will need to trigger the full screen overlay to view the captions.</p>
       <p>Use the link below to take a look!</p>
       </section>
